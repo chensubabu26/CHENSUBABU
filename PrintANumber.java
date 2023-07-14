9201eff713749d7ee698ecac67f2e14d54357cd7
chensubabu26 <60135039+chensubabu26@users.noreply.github.com>
2023-07-14 19:06:15 +0530

    Add files via upload

---
+++ PrintANumber.java
@@ -0,0 +1,11 @@
+package temp;
+import java.util.Scanner;
+
+public class PrintANumber {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		int number = reader.nextInt();
+		System.out.println("You entered: " + number);
+	}
+
+}
