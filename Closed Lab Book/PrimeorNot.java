a4a742daf53fb16a20971db8a97e3e27e4015220
chensubabu26 <60135039+chensubabu26@users.noreply.github.com>
2023-07-17 16:51:34 +0530

    Add files via upload

---
+++ Closed Lab Book/PrimeorNot.java
@@ -0,0 +1,36 @@
+package temp;
+
+import java.util.Scanner;
+
+public class PrimeorNot {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter a number: ");
+	        int number = scanner.nextInt();
+
+	        boolean isPrime = checkPrime(number);
+
+	        if (isPrime) {
+	            System.out.println(number + " is a prime number.");
+	        } else {
+	            System.out.println(number + " is not a prime number.");
+	        }
+
+	        scanner.close();
+	    }
+
+	    public static boolean checkPrime(int number) {
+	        if (number <= 1) {
+	            return false;
+	        }
+
+	        for (int i = 2; i <= Math.sqrt(number); i++) {
+	            if (number % i == 0) {
+	                return false;
+	            }
+	        }
+
+	        return true;
+	    }
+}
