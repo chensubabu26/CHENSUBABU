10b8364c3609f2204e0919747142c7993c558827
chensubabu26 <60135039+chensubabu26@users.noreply.github.com>
2023-07-17 17:21:45 +0530

    Add files via upload

---
+++ Closed Lab Book/NumberMatrixpattern.java
@@ -0,0 +1,20 @@
+package temp;
+
+public class NumberMatrixpattern {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1},
+            {2, 3},
+            {4, 5, 6},
+            {7, 8, 9, 10}
+        };
+
+        // Display the number matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
