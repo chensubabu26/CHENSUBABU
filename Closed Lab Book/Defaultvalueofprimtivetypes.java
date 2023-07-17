d26ef90ff3987c4d8ffb3ae0a8f7e967f353f24b
chensubabu26 <60135039+chensubabu26@users.noreply.github.com>
2023-07-17 16:32:18 +0530

    Add files via upload

---
+++ Closed Lab Book/Defaultvalueofprimtivetypes.java
@@ -0,0 +1,24 @@
+package temp;
+
+public class Defaultvalueofprimtivetypes {
+	    static boolean defaultBoolean;
+	    static byte defaultByte;
+	    static short defaultShort;
+	    static int defaultInt;
+	    static long defaultLong;
+	    static float defaultFloat;
+	    static double defaultDouble;
+	    static char defaultChar;
+
+	    public static void main(String[] args) {
+	        System.out.println("Default values of primitive types:");
+	        System.out.println("boolean: " + defaultBoolean);
+	        System.out.println("byte: " + defaultByte);
+	        System.out.println("short: " + defaultShort);
+	        System.out.println("int: " + defaultInt);
+	        System.out.println("long: " + defaultLong);
+	        System.out.println("float: " + defaultFloat);
+	        System.out.println("double: " + defaultDouble);
+	        System.out.println("char: " + defaultChar);
+	    }
+}
